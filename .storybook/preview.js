ec05abf8590d7363ebbfc16eb2d635e5c771760a
Casey Hunt <casey.hunt@gmail.com>
2021-02-05 00:55:50 -0600

    Closes #2: Embeds Figma + Storybook styling

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -9,6 +9,7 @@ export const decorators = [
   ),
 ];
 
-// export const parameters = {
-//   actions: { argTypesRegex: '^on[A-Z].*' },
-// };
+export const parameters = {
+  layout: 'centered',
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
--- src/stories/Textfield.stories.js
+++ src/stories/Textfield.stories.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Textfield from './Textfield';
+import { withDesign } from 'storybook-addon-designs';
 
 export default {
   title: 'TextField',
   component: Textfield,
+  decorators: [withDesign],
 };
 
 // const Template = args => (
@@ -16,6 +18,14 @@ export const Primary = args => <Textfield {...args} />;
 Primary.args = {
   label: 'Primary',
   helperText: 'This is helper text',
+};
+
+Primary.parameters = {
+  design: {
+    type: 'figma',
+    url:
+      'https://www.figma.com/file/5PkkZJIl0qWjcqNOJLsya6/Text-Field-Doc-Example?node-id=0%3A1',
+  },
 };
 
 // export const Primary = Template.bind({});
